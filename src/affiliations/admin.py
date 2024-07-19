31d3844da4bb3a84954cf61cd8e0f435e46b90ac
sanchegm <sanchegm@stanford.edu>
2024-07-19 15:43:40 -0700

    add coordinator model
    add coordinator model (name and email) and change data field interaction functionality when editing affiliation

--- src/affiliations/admin.py
+++ src/affiliations/admin.py
@@ -4,13 +4,27 @@
 from django.contrib import admin
 
 # In-house code:
-from affiliations.models import Affiliation
+from affiliations.models import Affiliation, Coordinator
+
+
+class CoordinatorInline(admin.TabularInline):
+    """Configure the coordinators admin panel"""
+    model = Coordinator
+    extra = 0
 
 
 class AffiliationsAdmin(admin.ModelAdmin):
     """Configure the affiliations admin panel."""
-
     search_fields = ["affiliation_id", "name"]
+    list_display = ["affiliation_id", "name", "status", "type", "family"]
+    fields = ("affiliation_id", "name", "status", "type", "family",
+              "approvers", "clinvar_submitter_ids", "members")
+    inlines = [CoordinatorInline]
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj is None:
+            return ["members",]
+        return ["affiliation_id", "members",]
 
 
 # Add models we want to be able to edit in the admin interface.
--- src/affiliations/models.py
+++ src/affiliations/models.py
@@ -9,7 +9,6 @@ class Affiliation(models.Model):
 
     affiliation_id: models.IntegerField = models.IntegerField()
     name: models.CharField = models.CharField()
-    coordinator: models.CharField = models.CharField()
     status: models.CharField = models.CharField()
     type: models.CharField = models.CharField()
     family: models.CharField = models.CharField()
@@ -20,3 +19,15 @@ class Affiliation(models.Model):
     def __str__(self):
         """Provide a string representation of an affiliation."""
         return f"Affiliation {self.affiliation_id} {self.name}"
+
+
+class Coordinator(models.Model):
+    """Define the shape of coordinator objects"""
+    affiliation = models.ForeignKey(
+        Affiliation, null=True, on_delete=models.CASCADE)
+    coordinator_name: models.CharField = models.CharField(max_length=200)
+    coordinator_email: models.EmailField = models.EmailField()
+
+    def __str__(self):
+        """Provide a string for header"""
+        return self.coordinator_name
