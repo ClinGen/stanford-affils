c00d18f31c53b18ea4f848eea4d3b8239d3d208c
sanchegm <sanchegm@stanford.edu>
2024-09-26 12:55:05 -0400

    refactor script

--- src/scripts/load.py
+++ src/scripts/load.py
@@ -1,139 +1,71 @@
+"""
+Script to be run to insert existing data from a spreadsheet to the database.
+"""
+
 import csv
-import os
 from affiliations.models import Affiliation, Submitter, Coordinator
 
 
-def run():
-    file = open(
-        "/Users/gabriellasanchez/Desktop/repos/stanford-affils/src/scripts/test_affils.csv")
-    csv_reader = csv.DictReader(file)
+def run():  # pylint: disable-msg=too-many-locals
+    """Iterate through CSV and create Affiliation, Submitter ID, and Coordinator
+    objects in the DB."""
 
-    Affiliation.objects.all().delete()
+    with open(
+        "/Users/gabriellasanchez/Desktop/repos/stanford-affils/src/scripts/test_affils.csv",
+        encoding="utf-8",
+    ) as file:
+        csv_reader = csv.DictReader(file)
 
-    for row in csv_reader:
-        external_full_name = row["Affiliation Full Name"]
-        affil_id = row["AffiliationID"]
-        coordinator_name = row["Coordinator(s)"].strip()
-        coordinator_email = row["Email"].strip()
-        clinvar_submitter_id = row["Submitter ID"]
-        vcep_ep_id = row["VCEP Affiliation ID"]
-        vcep_full_name = row["VCEP Affiliation Name"]
-        gcep_ep_id = row["GCEP Affiliation ID"]
-        gcep_full_name = row["GCEP Affiliation Name"]
-        status = row["Status"]
-        # cdwg = row["CDWG"]
+        Affiliation.objects.all().delete()
 
-        coordinator_names = coordinator_name.split(",")
-        coordinator_emails = coordinator_email.split(",")
+        for row in csv_reader:
+            external_full_name = row["Affiliation Full Name"].strip()
+            affil_id = row["AffiliationID"].strip()
+            coordinator_name = row["Coordinator(s)"].strip()
+            coordinator_email = row["Email"].strip()
+            clinvar_submitter_id = row["Submitter ID"]
+            vcep_ep_id = row["VCEP Affiliation ID"].strip()
+            vcep_full_name = row["VCEP Affiliation Name"].strip()
+            gcep_ep_id = row["GCEP Affiliation ID"].strip()
+            gcep_full_name = row["GCEP Affiliation Name"].strip()
+            status = row["Status"].strip()
+            # cdwg = row["CDWG"].strip()
 
-        if gcep_ep_id:
-            affil = Affiliation.objects.create(
-                affiliation_id=affil_id,
-                expert_panel_id=gcep_ep_id,
-                type="GCEP",
-                full_name=gcep_full_name,
-                status=status,
-            )
-            if clinvar_submitter_id != "":
-                Submitter.objects.create(
-                    affiliation=affil,
-                    clinvar_submitter_id=clinvar_submitter_id,
-                )
-            if coordinator_names or coordinator_emails:
-                for i in range(len(coordinator_names)):
-                    # If email is is less than name list, the name will be added
-                    # without any email fields.
-                    if len(coordinator_emails) > i:
-                        Coordinator.objects.create(
-                            affiliation=affil,
-                            coordinator_name=coordinator_names[i],
-                            coordinator_email=coordinator_emails[i],
-                        )
-                    else:
-                        Coordinator.objects.create(
-                            affiliation=affil,
-                            coordinator_name=coordinator_names[i],
-                        )
+            coordinator_names = coordinator_name.split(",")
+            coordinator_emails = coordinator_email.split(",")
 
-        if vcep_ep_id:
-            if "SC-VCEP" in vcep_full_name:
+            type_list = []
+            if gcep_ep_id:
+                type_list.append(("GCEP", gcep_ep_id, gcep_full_name))
+            if vcep_ep_id:
+                if "SC-VCEP" in vcep_full_name:
+                    type_list.append(("SC_VCEP", vcep_ep_id, vcep_full_name))
+                else:
+                    type_list.append(("VCEP", vcep_ep_id, vcep_full_name))
+            if not gcep_ep_id and not vcep_ep_id:
+                type_list.append(("INDEPENDENT_CURATION", None, external_full_name))
+
+            for type_name, ep_id, name in type_list:
                 affil = Affiliation.objects.create(
                     affiliation_id=affil_id,
-                    expert_panel_id=vcep_ep_id,
-                    type="SC_VCEP",
-                    full_name=vcep_full_name,
+                    expert_panel_id=ep_id,
+                    type=type_name,
+                    full_name=name,
                     status=status,
+                    # clinical_domain_working_group=cdwg,
                 )
                 if clinvar_submitter_id != "":
                     Submitter.objects.create(
                         affiliation=affil,
                         clinvar_submitter_id=clinvar_submitter_id,
                     )
-                if coordinator_names or coordinator_emails:
-                    for i in range(len(coordinator_names)):
-                        # If email is is less than name list, the name will be added
-                        # without any email fields.
-                        if len(coordinator_emails) > i:
-                            Coordinator.objects.create(
-                                affiliation=affil,
-                                coordinator_name=coordinator_names[i],
-                                coordinator_email=coordinator_emails[i],
-                            )
-                        else:
-                            Coordinator.objects.create(
-                                affiliation=affil,
-                                coordinator_name=coordinator_names[i],
-                            )
-            else:
-                affil = Affiliation.objects.create(
-                    affiliation_id=affil_id,
-                    expert_panel_id=vcep_ep_id,
-                    type="VCEP",
-                    full_name=vcep_full_name,
-                    status=status,
-                )
-                if clinvar_submitter_id != "":
-                    Submitter.objects.create(
-                        affiliation=affil,
-                        clinvar_submitter_id=clinvar_submitter_id,
-                    )
-                if coordinator_names or coordinator_emails:
-                    for i in range(len(coordinator_names)):
-                        # If email is is less than name list, the name will be added
-                        # without any email fields.
-                        if len(coordinator_emails) > i:
-                            Coordinator.objects.create(
-                                affiliation=affil,
-                                coordinator_name=coordinator_names[i],
-                                coordinator_email=coordinator_emails[i],
-                            )
-                        else:
-                            Coordinator.objects.create(
-                                affiliation=affil,
-                                coordinator_name=coordinator_names[i],
-                            )
-        if not gcep_ep_id and not vcep_ep_id:
-            affil = Affiliation.objects.create(
-                affiliation_id=affil_id,
-                expert_panel_id=None,
-                type="INDEPENDENT_CURATION",
-                full_name=external_full_name,
-                status=status,
-            )
-            if clinvar_submitter_id != "":
-                Submitter.objects.create(
-                    affiliation=affil,
-                    clinvar_submitter_id=clinvar_submitter_id,
-                )
-            if coordinator_names or coordinator_emails:
-                for i in range(len(coordinator_names)):
+                for i, name in enumerate(coordinator_names):
                     # If email is is less than name list, the name will be added
                     # without any email fields.
                     if len(coordinator_emails) > i:
-
                         Coordinator.objects.create(
                             affiliation=affil,
-                            coordinator_name=coordinator_names[i],
+                            coordinator_name=name,
                             coordinator_email=coordinator_emails[i],
                         )
                     else:
