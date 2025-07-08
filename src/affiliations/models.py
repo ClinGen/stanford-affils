33aab95c261f53ca52991d3bd4b009c38dd5d178
sanchegm <sanchegm@stanford.edu>
2025-07-08 11:42:18 -0700

    Tests and linting
    - Add CDWG API tests

--- src/affiliations/models.py
+++ src/affiliations/models.py
@@ -4,6 +4,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+
 class AffiliationStatus(models.TextChoices):  # pylint: disable=too-many-ancestors
     """Creating choices for status."""
 
