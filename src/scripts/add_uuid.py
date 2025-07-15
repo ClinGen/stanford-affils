b7106c9f571fc8d63a206f2cc26c1deb18e0709d
sanchegm <sanchegm@stanford.edu>
2025-07-15 12:17:08 -0700

    Add UUID migration script
    - Create migration script for existing UUIDs on existing Affiliations.

---
+++ src/scripts/add_uuid.py
@@ -0,0 +1,61 @@
+"""
+Script to be run to insert existing UUID data from 
+a spreadsheet to the database.
+
+CSV needs to be saved in the `scripts` folder in directory before running.
+
+You can then run this script by running: 
+`python manage.py runscript add_uuid` in the command line from the directory.
+
+Follow steps outlined in [tutorial.md](
+doc/tutorial.md/#running-the-loadpy-script-to-import-data-into-the-database).
+"""
+
+import csv
+import uuid
+from pathlib import Path
+
+from django.db import transaction
+from affiliations.models import Affiliation
+
+CSV_FILE = Path(__file__).parent / "gpm-expert-panel_custom.csv"
+
+
+@transaction.atomic
+def run():
+    """
+    Iterate through a CSV, check if the ep_id and type match CSV,
+    then add UUID to affiliation.
+    """
+    with open(CSV_FILE, encoding="utf-8-sig") as file:
+        reader = csv.DictReader(file)
+        updated = 0
+        missing = []
+
+        for row in reader:
+            uuid_value = row["uuid"].strip()
+            expert_panel_id = row["expert_panel_id"].strip()
+            type_value = row["type"].strip()
+
+            try:
+                affil = Affiliation.objects.get(
+                    expert_panel_id=expert_panel_id,
+                    type=type_value,
+                )
+                try:
+                    affil.uuid = uuid.UUID(uuid_value)
+                    affil.save()
+                    updated += 1
+                except ValueError:
+                    print(
+                        f"Invalid UUID format for ID {expert_panel_id},",
+                        "Type {type_value}: {uuid_value}",
+                    )
+            except Affiliation.DoesNotExist:
+                missing.append((expert_panel_id, type_value))
+
+        print(f"{updated} affiliations updated with UUIDs.")
+        if missing:
+            print(f"{len(missing)} rows skipped (no match found):")
+            for expert_panel_id, type_value in missing:
+                print(f" - ID: {expert_panel_id}, Type: {type_value}")
